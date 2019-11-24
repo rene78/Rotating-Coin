44eb6c5c6fa3c2d821039a59230bcc0fe11cfd4e
Rene <36115705+rene78@users.noreply.github.com>
2019-11-25 00:41:37 +0100

    three() as closure

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-let scene, camera, renderer, coin, id;
 
-document.addEventListener("DOMContentLoaded", three);
+const swissFranc = three();
 
 function three() {
-  initializeScene()
+  let scene, camera, renderer, coin, id, angleToVertical;
+  initializeScene();
 
   // Adding ambient lighting
   scene.add(new THREE.AmbientLight(0xffffff, 0.5));
@@ -63,65 +63,73 @@ function three() {
   coin.rotation.x = Math.PI / 2;
   coin.rotation.y = Math.PI / 2;
 
-  animate();
-}
+  animateCoin();
 
-function animate() {
-  coin.rotation.x += 0.05;
-  xRotAngle = coin.rotation.x;
-  id = requestAnimationFrame(animate);
-  // console.log(id);
-  // console.log(xRotAngle);
+  function animateCoin() {
+    coin.rotation.x += 0.05;
+    xRotAngle = coin.rotation.x;
+    id = requestAnimationFrame(animateCoin);
+    // console.log(id);
+    // console.log(xRotAngle);
 
-  //Change camera position during animation
-  // let dist = 7;
-  // dist = dist + (dist * id / 1000);
-  // console.log(dist);
-  // camera.position.set(0, 0, dist);
+    //Change camera position during animation
+    // let dist = 7;
+    // dist = dist + (dist * id / 1000);
+    // console.log(dist);
+    // camera.position.set(0, 0, dist);
 
-  renderer.render(scene, camera);
-}
-
-function initializeScene() {
-  let div = document.querySelector(".three-coin");
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(75, div.offsetWidth / div.offsetHeight, 0.1, 1000);
-  renderer = new THREE.WebGLRenderer({ alpha: true });
-  renderer.setSize(div.offsetWidth, div.offsetHeight);
-  div.appendChild(renderer.domElement);
-  // document.body.appendChild(renderer.domElement);
-  scene.background = new THREE.Color(0xffffff);
-}
-
-let angleToVertical;
-function stopAnimation(side) {
-  //on first run of function "side" is a string ("heads" or "tails"). On subsequent runs it is a timestamp (number)
-  if (typeof side === "string") {
-    // console.log(side);
-    angleToVertical = (side === "tails") ? (3 * Math.PI / 2) : (Math.PI / 2);//tails=1.5pi, heads=pi/2
-    // console.log(angleToVertical);
+    renderer.render(scene, camera);
   }
-  
-  let rotVal = coin.rotation.x;
 
-  // console.log(rotVal);
-  let deltaAngle = rotVal % (Math.PI * 2) - angleToVertical;
-  // console.log(deltaAngle);
-
-  if (deltaAngle < 0.06 && deltaAngle > -0.06) {
-    cancelAnimationFrame(id);//cancel coin animation
-    cancelAnimationFrame(stopAnimation);//cancel excution of this function
-    return;
+  function initializeScene() {
+    let div = document.querySelector(".three-coin");
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, div.offsetWidth / div.offsetHeight, 0.1, 1000);
+    renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setSize(div.offsetWidth, div.offsetHeight);
+    div.appendChild(renderer.domElement);
+    // document.body.appendChild(renderer.domElement);
+    scene.background = new THREE.Color(0xffffff);
   }
-  requestAnimationFrame(stopAnimation);//rerun this function until if-statment above is true
+
+  function stopAnimation(side) {
+    //on first run of function "side" is a string ("heads" or "tails"). On subsequent runs it is a timestamp (number)
+    if (typeof side === "string") {
+      // console.log(side);
+      angleToVertical = (side === "tails") ? (3 * Math.PI / 2) : (Math.PI / 2);//tails=1.5pi, heads=pi/2
+      // console.log(angleToVertical);
+    }
+
+    let rotVal = coin.rotation.x;
+
+    // console.log(rotVal);
+    let deltaAngle = rotVal % (Math.PI * 2) - angleToVertical;
+    // console.log(deltaAngle);
+
+    if (deltaAngle < 0.06 && deltaAngle > -0.06) {
+      cancelAnimationFrame(id);//cancel coin animation
+      cancelAnimationFrame(stopAnimation);//cancel excution of this function
+      return;
+    }
+    requestAnimationFrame(stopAnimation);//rerun this function until if-statment above is true
+  }
+
+  const coinObj = {
+    stopAnimation,
+    animateCoin
+  }
+  return coinObj;
 }
 
+/*
 function startAnimation() {
-  //Replace current three.js canvas ("three-coin") with an empty one. Else new one is created
-  var newdiv = document.createElement("div");
-  newdiv.setAttribute("class", "three-coin");
-  var oldDiv = document.querySelector(".three-coin");
-  var mainBody = document.querySelector("main");
-  mainBody.replaceChild(newdiv, oldDiv);
-  three();
+  //Replace current three.js canvas ("three-coin") with an empty one. Else a new one is created
+  // let newdiv = document.createElement("div");
+  // newdiv.setAttribute("class", "three-coin");
+  // let oldDiv = document.querySelector(".three-coin");
+  // let mainBody = document.querySelector("main");
+  // mainBody.replaceChild(newdiv, oldDiv);
+  // three();
+  animateCoin();
 }
+*/
